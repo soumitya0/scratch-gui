1dd7383592887c9ed1d531a8989df9b331f72169
soumitya <soumityachauhan@gmail.com>
2020-11-28 11:59:58 +0530

    ADDED - TESTING MENU IN TUTORIAL OPPTION

--- src/containers/backdrop-library.jsx
+++ src/containers/backdrop-library.jsx
@@ -1,41 +1,38 @@
-import bindAll from 'lodash.bindall';
-import PropTypes from 'prop-types';
-import React from 'react';
-import {defineMessages, injectIntl, intlShape} from 'react-intl';
-import VM from 'scratch-vm';
+import bindAll from "lodash.bindall";
+import PropTypes from "prop-types";
+import React from "react";
+import { defineMessages, injectIntl, intlShape } from "react-intl";
+import VM from "scratch-vm";
 
-import backdropLibraryContent from '../lib/libraries/backdrops.json';
-import backdropTags from '../lib/libraries/backdrop-tags';
-import LibraryComponent from '../components/library/library.jsx';
+import backdropLibraryContent from "../lib/libraries/backdrops.json";
+import backdropTags from "../lib/libraries/backdrop-tags";
+import LibraryComponent from "../components/library/library.jsx";
 
 const messages = defineMessages({
     libraryTitle: {
-        defaultMessage: 'Choose a Backdrop',
-        description: 'Heading for the backdrop library',
-        id: 'gui.costumeLibrary.chooseABackdrop'
-    }
+        defaultMessage: "Choose a Backdrop",
+        description: "Heading for the backdrop library",
+        id: "gui.costumeLibrary.chooseABackdrop",
+    },
 });
 
-
 class BackdropLibrary extends React.Component {
-    constructor (props) {
+    constructor(props) {
         super(props);
-        bindAll(this, [
-            'handleItemSelect'
-        ]);
+        bindAll(this, ["handleItemSelect"]);
     }
-    handleItemSelect (item) {
+    handleItemSelect(item) {
         const vmBackdrop = {
             name: item.name,
             rotationCenterX: item.rotationCenterX,
             rotationCenterY: item.rotationCenterY,
             bitmapResolution: item.bitmapResolution,
-            skinId: null
+            skinId: null,
         };
         // Do not switch to stage, just add the backdrop
         this.props.vm.addBackdrop(item.md5ext, vmBackdrop);
     }
-    render () {
+    render() {
         return (
             <LibraryComponent
                 data={backdropLibraryContent}
@@ -52,7 +49,7 @@ class BackdropLibrary extends React.Component {
 BackdropLibrary.propTypes = {
     intl: intlShape.isRequired,
     onRequestClose: PropTypes.func,
-    vm: PropTypes.instanceOf(VM).isRequired
+    vm: PropTypes.instanceOf(VM).isRequired,
 };
 
 export default injectIntl(BackdropLibrary);
